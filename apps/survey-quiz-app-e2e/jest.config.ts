1d4aa3df5fe3469d08cb2c2bfb8ace964103b304
sangtrandev00 <nhatsang0101@gmail.com>
2025-02-08 10:36:02 +0700

    Initial commit
    To connect your workspace to Nx Cloud, push your repository
    to your git hosting provider and go to the following URL:
    
    https://cloud.nx.app/connect/7RU0Jpsee3

---
+++ apps/survey-quiz-app-e2e/jest.config.ts
@@ -0,0 +1,15 @@
+export default {
+  displayName: 'survey-quiz-app-e2e',
+  preset: '../../jest.preset.js',
+  globalSetup: '<rootDir>/src/support/global-setup.ts',
+  globalTeardown: '<rootDir>/src/support/global-teardown.ts',
+  setupFiles: ['<rootDir>/src/support/test-setup.ts'],
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/survey-quiz-app-e2e',
+};
---
+++ apps/survey-quiz-app/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'survey-quiz-app',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }]
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/survey-quiz-app'
+};
